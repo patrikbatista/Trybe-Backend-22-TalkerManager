2e3fa26a76b5333c8d417e7efb53233efe8756a9
patrik <pcr_batista@hotmail.com>
2022-06-14 15:53:41 -0300

    implementa rota get talkers

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-// const rescue = require('express-rescue');
+const rescue = require('express-rescue');
+
+const controller = require('../controllers');
+// const middlewares = require('../middlewares');
 
 const talker = Router();
 talker.post('/', (req, res, _next) => res.end());
-talker.get('/', (req, res, _next) => res.end());
+talker.get('/', rescue(controller.getAllTalkers));
 talker.get('/id', (req, res, _next) => res.end());
 talker.put('/id', (req, res, _next) => res.end());
 talker.delete('/id', (req, res, _next) => res.end());
