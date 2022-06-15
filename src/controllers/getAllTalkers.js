1f8dc24ed810baa7dd51a3d877bc27a614c19a02
patrik <pcr_batista@hotmail.com>
2022-06-15 13:42:53 -0300

    :recycle: refactor: remove console.log dp getAllTalkers e modifica talkers.json

--- src/controllers/getAllTalkers.js
+++ src/controllers/getAllTalkers.js
@@ -3,7 +3,6 @@ const schemas = require('../schemas');
 
 module.exports = async (req, res) => {
   const talkers = await services.getTalkers();
-  console.log(talkers);
   const result = schemas.allTalkers(talkers);
   
   return res.status(200).json(result.data);
