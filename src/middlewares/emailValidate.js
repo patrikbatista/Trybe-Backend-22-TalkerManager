85f447b63e5199623bed6db1f1740479a0167093
patrik <pcr_batista@hotmail.com>
2022-06-15 18:16:33 -0300

    :bug: fix: reafatora middleware emailValidate que apresentava erro

--- src/middlewares/emailValidate.js
+++ src/middlewares/emailValidate.js
@@ -2,11 +2,11 @@ const schemas = require('../schemas');
 
 module.exports = (req, res, next) => {
   const { email } = req.body;
-  const isEmail = schemas.isEmail(email);
-
-  if (!email) {
+  
+  if (email === undefined) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+  const isEmail = schemas.isEmail(email);
 
   if (!isEmail) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
