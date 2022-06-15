c6e252dfbd9c2256572cef99663b864da57966ad
patrik <pcr_batista@hotmail.com>
2022-06-15 16:02:12 -0300

    :recycle: refactor: refatora o index adicianando caminho das funcoes isEmail e isPassword

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,5 +1,9 @@
 const allTalkers = require('./allTalkers');
+const isEmail = require('./isEmail');
+const isPassword = require('./isPassword');
 
 module.exports = {
   allTalkers,
+  isEmail,
+  isPassword,
 };
