b6054412056782d6b6a56bb7abf3535de09f67aa
patrik <pcr_batista@hotmail.com>
2022-06-16 15:31:43 -0300

    :recycle: refactor: Adicona caminho do getTalkerId

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const getAllTalkers = require('./getAllTalkers');
 const login = require('./login');
+const getTalkerId = require('./getTalkerId');
 
 module.exports = {
   getAllTalkers,
   login,
+  getTalkerId,
 };
