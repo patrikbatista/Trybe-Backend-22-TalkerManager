a2fb6908573554e89a15700651e09d26dc27fa77
patrik <pcr_batista@hotmail.com>
2022-06-20 17:34:51 -0300

    :recycle: refactor: Adicionando novos caminhos de arquivos

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,9 +1,15 @@
 const allTalkers = require('./allTalkers');
 const isEmail = require('./isEmail');
 const isPassword = require('./isPassword');
+const isAge = require('./isAge');
+const isName = require('./isName');
+const isToken = require('./isToken');  
 
 module.exports = {
   allTalkers,
   isEmail,
   isPassword,
+  isToken,
+  isName,
+  isAge,
 };
