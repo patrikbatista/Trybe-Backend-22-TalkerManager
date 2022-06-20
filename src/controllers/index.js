1582db2a46f3250d698ff1e19e2eb05884b76d48
patrik <pcr_batista@hotmail.com>
2022-06-20 19:57:47 -0300

    :recycle: refactor: Inclui caminho do arquivo controller postTalker

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const getAllTalkers = require('./getAllTalkers');
 const login = require('./login');
 const getTalkerId = require('./getTalkerId');
+const postTalker = require('./postTalker');
 
 module.exports = {
   getAllTalkers,
   login,
   getTalkerId,
+  postTalker,
 };
