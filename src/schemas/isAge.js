4454fc35ad1c6749d0b244963f77b741dd698ea2
patrik <pcr_batista@hotmail.com>
2022-06-20 19:22:59 -0300

    :recycle: refactor: Altera objeto retornado

--- src/schemas/isAge.js
+++ src/schemas/isAge.js
@@ -1,12 +1,14 @@
 module.exports = (age) => {
   if (!age) {
     return { 
+      status: 400,
       data: 'O campo "age" é obrigatório',
       response: false,  
   }; 
   }
   if (age.length < 18) {
     return { 
+      status: 400,
       data: 'A pessoa palestrante deve ser maior de idade', 
       response: false,  
     };
