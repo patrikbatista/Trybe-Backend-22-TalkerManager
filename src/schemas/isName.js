714b6015a62108419ae89060f54d262f5ef7ff22
patrik <pcr_batista@hotmail.com>
2022-06-20 17:00:05 -0300

    :sparkles: feat: funcao de validacao de nome

---
+++ src/schemas/isName.js
@@ -0,0 +1,13 @@
+module.exports = (name) => {
+  if (!name) {
+    return { data: 'O campo "name" é obrigatório',
+    response: false,  
+  }; 
+  }
+  if (name.length < 3) {
+    return { 
+      data: 'O "name" deve ter pelo menos 3 caracteres', 
+      response: false,  
+    };
+  }
+};
