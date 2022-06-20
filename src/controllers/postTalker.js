da74f24fccf1a67244ea7830f54ad205c402fabe
patrik <pcr_batista@hotmail.com>
2022-06-20 19:55:16 -0300

    :sparkles: feat: implementa controller postTalker

---
+++ src/controllers/postTalker.js
@@ -0,0 +1,18 @@
+const fs = require('fs/promises');
+const services = require('../services');
+
+module.exports = async (req, res) => {
+  const talkers = await services.getTalkers();
+  const { name, age, talk } = req.body;
+  const id = talkers.length + 1; 
+
+  const newTalk = {                                   
+    id, 
+    name, 
+    age, 
+    talk,
+  };
+  talkers.push(newTalk);
+  fs.writeFile('./talker.json', JSON.stringify(talkers));
+  res.status(201).json(newTalk);
+};
