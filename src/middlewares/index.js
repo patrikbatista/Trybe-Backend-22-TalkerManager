4d55853acb9b687df1a2294b443e071e264ea05a
patrik <pcr_batista@hotmail.com>
2022-06-20 19:43:13 -0300

    :recycle: refactor: Inclui caminho para middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,19 @@
 const error = require('./error');
 const emailValidate = require('./emailValidate');
 const passwordValidate = require('./passwordValidate');
+const ageValidate = require('./ageValidate');
+const nameValidate = require('./nameValidate');
+const rateValidate = require('./rateValidate');
+const talkValidate = require('./talkValidate');
+const tokenValidate = require('./tokenValidate');
 
 module.exports = {
   error,
   emailValidate,
   passwordValidate,
+  ageValidate,
+  nameValidate,
+  rateValidate,
+  talkValidate,
+  tokenValidate,
 };
