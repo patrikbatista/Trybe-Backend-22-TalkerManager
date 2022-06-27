a8df08b6f70bef14aedfad9ce936b002a674cf9d
patrik <pcr_batista@hotmail.com>
2022-06-27 10:16:32 -0300

    refactor: remove complexibilidade do talkValidate

--- src/middlewares/rateValidate.js
+++ src/middlewares/rateValidate.js
@@ -3,8 +3,10 @@ const schemas = require('../schemas');
 module.exports = (req, res, next) => {
   const { talk } = req.body;
   const { rate } = talk;
+  
+  if (rate === undefined) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  
   const validatedRate = schemas.isRate(rate);
-
   if (!validatedRate) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
--- src/middlewares/talkValidate.js
+++ src/middlewares/talkValidate.js
@@ -6,14 +6,6 @@ module.exports = (req, res, next) => {
       .json({ 
         message: 'O campo "talk" é obrigatório' });
     }
-    const { watchedAt, rate } = talk;
-    if (rate === undefined) return res.status(400).json({ message: 'O campo "rate" é obrigatório' }); 
-    if (!watchedAt) return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
-  
-    if (watchedAt === undefined || rate === undefined) {
-      return res.status(400)
-      .json({ 
-        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-    }
+    
     next();
   };
