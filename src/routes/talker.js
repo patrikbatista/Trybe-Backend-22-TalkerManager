7186d848e6e125847b16cf409515778a7bf35d30
patrik <pcr_batista@hotmail.com>
2022-06-27 07:03:26 -0300

    :recycle: refactor: Refatora codigo, implementando talker.use

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -4,17 +4,20 @@ const controller = require('../controllers');
 const middlewares = require('../middlewares');
 
 const talker = Router();
-talker.post('/', 
+talker.get('/', controller.getAllTalkers);
+talker.get('/:id', controller.getTalkerId);
+talker.delete('/id', (req, res, _next) => res.end());
+talker.use(
   middlewares.tokenValidate,
   middlewares.nameValidate,
   middlewares.ageValidate,
   middlewares.talkValidate,
   middlewares.watchedAtValidate,
   middlewares.rateValidate,
+);
+talker.post('/', 
   controller.postTalker);
-talker.get('/', controller.getAllTalkers);
-talker.get('/:id', controller.getTalkerId);
-talker.put('/id', (req, res, _next) => res.end());
-talker.delete('/id', (req, res, _next) => res.end());
+talker.put('/id', 
+  middlewares.rateValidate);
 
 module.exports = talker;
